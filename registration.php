fb9147eb070164eabc4d3eaa4d5e8042259e593b
Mark Shust <mark@shust.com>
2022-04-04 11:08:49 -0400

    Magento module registration file

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Macademy_Blog',
+    __DIR__,
+);
