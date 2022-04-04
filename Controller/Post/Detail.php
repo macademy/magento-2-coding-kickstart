def6242e9bc926a78e5cdec546c5ea249f277586
Mark Shust <mark@shust.com>
2022-04-04 14:20:40 -0400

    Create a new object in Magento using Object Manager

--- Controller/Post/Detail.php
+++ Controller/Post/Detail.php
@@ -4,13 +4,16 @@ namespace Macademy\Blog\Controller\Post;
 
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\ObjectManager;
 
 class Detail implements HttpGetActionInterface
 {
     public function execute()
     {
+        $om = ObjectManager::getInstance();
+        $session = $om->get(Session::class);
         echo '<pre>';
-        var_dump(new Session());
+        var_dump($session->getData());
         die();
     }
 }
