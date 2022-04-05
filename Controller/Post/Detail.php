5a795db8540666f64712bd8334e1df57c93ddfa1
Mark Shust <mark@shust.com>
2022-04-05 09:15:35 -0400

    Create an object with dependency injection using an interface

--- Controller/Post/Detail.php
+++ Controller/Post/Detail.php
@@ -4,17 +4,20 @@ namespace Macademy\Blog\Controller\Post;
 
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\RequestInterface;
 
 class Detail implements HttpGetActionInterface
 {
     public function __construct(
         private Session $session,
+        private RequestInterface $request,
     ) {}
 
     public function execute()
     {
         echo '<pre>';
         var_dump($this->session->getData());
+        var_dump($this->request->getParams());
         die();
     }
 }
