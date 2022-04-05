1884020ba9eb6d5fd9e37e4bea8a6d9752504047
Mark Shust <mark@shust.com>
2022-04-05 09:52:27 -0400

    Forward a Magento controller action with a factory

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -3,18 +3,19 @@
 namespace Macademy\Blog\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\Controller\Result\Redirect;
-use Magento\Framework\Controller\Result\RedirectFactory;
+use Magento\Framework\Controller\Result\Forward;
+use Magento\Framework\Controller\Result\ForwardFactory;
 
 class Index implements HttpGetActionInterface
 {
     public function __construct(
-        private RedirectFactory $redirectFactory,
+        private ForwardFactory $forwardFactory,
     ) {}
 
-    public function execute(): Redirect
+    public function execute(): Forward
     {
-        $redirect = $this->redirectFactory->create();
-        return $redirect->setPath('*/post/list');
+        /** @var Forward $forward */
+        $forward = $this->forwardFactory->create();
+        return $forward->setController('post')->forward('list');
     }
 }
