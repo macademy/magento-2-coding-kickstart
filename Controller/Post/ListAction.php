a74f24de52e536e930e7c2364a7d34bc7ac40921
Mark Shust <mark@shust.com>
2022-04-05 09:26:53 -0400

    Create a page response for the blog post list view

--- Controller/Post/ListAction.php
+++ Controller/Post/ListAction.php
@@ -3,11 +3,17 @@
 namespace Macademy\Blog\Controller\Post;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
 
 class ListAction implements HttpGetActionInterface
 {
-    public function execute()
+    public function __construct(
+        private PageFactory $pageFactory
+    )  {}
+
+    public function execute(): Page
     {
-        die('Blog post list');
+        return $this->pageFactory->create();
     }
 }
