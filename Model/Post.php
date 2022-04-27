6c39e5bae7a367cf4530078bbf91388d41f2dbe1
Mark Shust <mark@shust.com>
2022-04-27 15:05:29 -0400

    Update a model to implement a DTO interface

--- Model/Post.php
+++ Model/Post.php
@@ -2,12 +2,38 @@
 
 namespace Macademy\Blog\Model;
 
+use Macademy\Blog\Api\Data\PostInterface;
 use Magento\Framework\Model\AbstractModel;
 
-class Post extends AbstractModel
+class Post extends AbstractModel implements PostInterface
 {
     protected function _construct()
     {
         $this->_init(ResourceModel\Post::class);
+    }
+
+    public function getTitle()
+    {
+        return $this->getData(self::TITLE);
+    }
+
+    public function setTitle($title)
+    {
+        return $this->setData(self::TITLE, $title);
+    }
+
+    public function getContent()
+    {
+        return $this->getData(self::CONTENT);
+    }
+
+    public function setContent($content)
+    {
+        return $this->setData(self::CONTENT, $content);
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->getData(self::CREATED_AT);
     }
 }
