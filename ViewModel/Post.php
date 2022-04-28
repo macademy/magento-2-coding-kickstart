bc75206dbbcf3256ed38f239f4785f57ac9fae27
Mark Shust <mark@shust.com>
2022-04-28 15:19:53 -0400

    Load data using a repository

--- ViewModel/Post.php
+++ ViewModel/Post.php
@@ -2,13 +2,18 @@
 
 namespace Macademy\Blog\ViewModel;
 
+use Macademy\Blog\Api\Data\PostInterface;
+use Macademy\Blog\Api\PostRepositoryInterface;
 use Macademy\Blog\Model\ResourceModel\Post\Collection;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class Post implements ArgumentInterface
 {
     public function __construct(
         private Collection $collection,
+        private PostRepositoryInterface $postRepository,
+        private RequestInterface $request,
     ) {}
 
     public function getList(): array
@@ -19,5 +24,11 @@ class Post implements ArgumentInterface
     public function getCount(): int
     {
         return $this->collection->count();
+    }
+
+    public function getDetail(): PostInterface
+    {
+        $id = (int) $this->request->getParam('id');
+        return $this->postRepository->getById($id);
     }
 }
