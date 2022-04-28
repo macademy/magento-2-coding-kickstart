abb98a71ebac3241318edeb60874e1ac37012d95
Mark Shust <mark@shust.com>
2022-04-28 11:47:01 -0400

    Write another setup data patch script

---
+++ Setup/Patch/Data/PopulateBlogPosts1.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\Blog\Setup\Patch\Data;
+
+use Macademy\Blog\Api\PostRepositoryInterface;
+use Macademy\Blog\Model\PostFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchInterface;
+
+class PopulateBlogPosts1 implements DataPatchInterface
+{
+    public function __construct(
+        private ModuleDataSetupInterface $moduleDataSetup,
+        private PostFactory $postFactory,
+        private PostRepositoryInterface $postRepository,
+    ) {}
+
+    public static function getDependencies(): array
+    {
+        return [];
+    }
+
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    public function apply()
+    {
+        $this->moduleDataSetup->startSetup();
+
+        $posts = [
+            [
+                'title' => 'Today is sunny',
+                'content' => 'The weather has been great all week.',
+            ],
+            [
+                'title' => 'My movie review',
+                'content' => 'I give this movie 5 out of 5 stars!',
+            ],
+        ];
+
+        foreach ($posts as $postData) {
+            $post = $this->postFactory->create();
+            $post->setData($postData);
+            $this->postRepository->save($post);
+        }
+
+        $this->moduleDataSetup->endSetup();
+    }
+}
